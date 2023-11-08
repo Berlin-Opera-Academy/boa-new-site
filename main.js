167602598ac2b0abe71b531a439b2202c42cb3fe
berlinoperaacademy <43492991+berlinoperaacademy@users.noreply.github.com>
2023-11-08 18:16:21 +0100

    hamburger improve

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ const navMenu = document.querySelector('.nav-menu');
 hamburgerMenu.addEventListener('click', () => {
   // Toggle the 'active' class on the navigation menu
   navMenu.classList.toggle('active');
+  
+  // Check if the navigation menu has the 'active' class
+  if (navMenu.classList.contains('active')) {
+    // If it does, set the display to 'flex'
+    navMenu.style.display = 'flex';
+  } else {
+    // If it doesn't, set the display to 'none'
+    navMenu.style.display = 'none';
+  }
 });
