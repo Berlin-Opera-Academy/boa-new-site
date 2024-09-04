45d2a7641cd8852e4fb636c3d243250fb0366248
John Moorman <45500240+mojoro@users.noreply.github.com>
2024-09-04 15:12:13 +0200

    Update nav-menu to close if clicked outside

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -2,22 +2,35 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
 
-// Add a click event listener to the hamburger menu
-hamburgerMenu.addEventListener("click", () => {
-  // Toggle the 'active' class on the navigation menu
+// Function to toggle the navigation menu
+function toggleNavMenu() {
   navMenu.classList.toggle("active");
 
-  // Check if the navigation menu has the 'active' class
   if (navMenu.classList.contains("active")) {
-    // If it does, set the display to 'flex'
     navMenu.style.display = "flex";
   } else {
-    // If it doesn't, set the display to 'none'
     navMenu.style.display = "none";
   }
+}
+
+// Add a click event listener to the hamburger menu
+hamburgerMenu.addEventListener("click", () => {
+  toggleNavMenu();
+  hamburgerMenu.classList.toggle("open");
 });
-document
-  .querySelector(".hamburger-menu")
-  .addEventListener("click", function () {
-    this.classList.toggle("open");
-  });
+
+// Add a click event listener to the document
+document.addEventListener("click", (event) => {
+  // Check if the click is outside the navigation menu and hamburger menu
+  if (
+    !navMenu.contains(event.target) &&
+    !hamburgerMenu.contains(event.target)
+  ) {
+    // If the navigation menu is active, hide it
+    if (navMenu.classList.contains("active")) {
+      navMenu.classList.remove("active");
+      navMenu.style.display = "none";
+      hamburgerMenu.classList.remove("open");
+    }
+  }
+});
